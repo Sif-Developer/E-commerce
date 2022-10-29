f5d7927b1caef72f67a130c09f818fd73055f839
Miguel <mirkouda@gmail.com>
2022-10-29 16:46:11 +0200

    adición endpoints

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,7 +1,8 @@
 const db = require("../config/database");
+// const categories = [{}] 
 
 const CategoryController = {
-    createCategory(req, res) {
+        createCategory(req, res) {
         let sql = `INSERT INTO categories (name) values
     ('${req.body.name}');`;
         db.query(sql, (err, result) => {
@@ -9,6 +10,27 @@ const CategoryController = {
             res.send("Category added...");
         });
     },
+
+    showCategories(req, res) {
+        let sql = `SELECT * FROM categories`; //mostrar productos
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            res.send(result)
+        })
+    },
+    
+    // selectCategoryByName(req, res) {
+    //     res.send(categories.filter(category => category.name == req.params.name))
+    // },
+
+    selectCategoryById(req, res) {
+        let sql = `SELECT * FROM categories WHERE id = ${req.params.id}`;
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        })
+    },
+    
 }
 
 
--- routes/categories.js
+++ routes/categories.js
@@ -4,6 +4,9 @@ const db = require("../config/database");
 const CategoryController = require("../controllers/CategoryController");
 
 router.post("/createCategory", CategoryController.createCategory);
+router.get("/showCategories", CategoryController.showCategories);
+// router.get("/selectCategoryByName/:name",CategoryController.selectCategoryByName);
+router.get("/selectCategoryById/:id", CategoryController.selectCategoryById);
 
 
 
