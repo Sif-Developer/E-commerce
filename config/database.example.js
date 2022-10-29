efe7e126d5b7e85ac8ca387008eb92bf072a0aa9
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-10-29 12:33:57 +0200

    folders created

--- config/database.example.js
+++ config/database.example.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'admin123',
+  password: 'write your password',
 });
 
 db.connect();
