f61f454b03e65c75454463320cf912f8e464d662
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-10-29 12:14:16 +0200

    created port and folder config added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("Holita")
+const exp = require("constants");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const db = require("./config/database");
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server started successfully in port 3000");
+});
